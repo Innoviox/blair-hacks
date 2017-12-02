ecbd0341dc4ff58eab3371cd66613a488976f031
Innoviox <sarras305@gmail.com>
2017-12-02 14:44:44 -0500

    healthbar

--- Tanks/src/Damagable.java
+++ Tanks/src/Damagable.java
@@ -3,10 +3,12 @@ import java.awt.image.BufferedImage;
 
 public abstract class Damagable extends Polygon {
     public int health, max_health;
+    private HealthBar hb;
     public Damagable(Point[] inShape, Point inPosition, double inRotation, int width, int height, BufferedImage image, int health) {
         super(inShape, inPosition, inRotation, width, height, image);
         this.health = health;
         this.max_health = health;
+        this.hb = new HealthBar(this);
     }
 
     public void updateHealth(int newHealth) {
@@ -14,34 +16,10 @@ public abstract class Damagable extends Polygon {
         this.max_health = newHealth;
     }
 
-    public void paintHealthBar(Graphics brush) {
-        int[] xPoints = new int[4];
-        int[] yPoints = new int[4];
-        int back = (int)position.x;// - (width / 2);
-        xPoints[0] = back;
-        xPoints[1] = back;
-        xPoints[2] = back + max_health;
-        xPoints[3] = back + max_health;
-
-        int bottom = (int)position.y;// - height;
-        yPoints[0] = bottom;
-        yPoints[1] = bottom + 10;
-        yPoints[2] = bottom + 10;
-        yPoints[3] = bottom;
-
-        brush.setColor(Color.black);
-        brush.drawPolygon(xPoints, yPoints, 4);
-
-        xPoints[2] = back + health;
-        xPoints[3] = back + health;
-
-        brush.setColor(color.green);
-        brush.fillPolygon(xPoints, yPoints, 4);
-    }
 
     @Override
     public void paint(Graphics brush) {
         super.paint(brush);
-        paintHealthBar(brush);
+        this.hb.paint(brush);
     }
 }
--- Tanks/src/HealthBar.java
+++ Tanks/src/HealthBar.java
@@ -1,6 +1,67 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class HealthBar extends Polygon {
     private Damagable obj;
+    private static final int BASE = -20;
+    private static final Point[] BAR_SHAPE = new Point[] {
+            new Point(0, 0),
+            new Point(0, 10),
+            new Point(10, 10),
+            new Point(10, 0)
+    };
     public HealthBar(Damagable obj) {
+        super(BAR_SHAPE, obj.position, obj.rotation, obj.max_health, obj.height, null);
+        this.shape[2].x = obj.max_health;
+        this.shape[3].x = obj.max_health;
         this.obj = obj;
+    }
+
+    @Override
+    public void move() {
+        this.rotation = obj.rotation;
+    }
+
+    @Override
+    public Polygon[] destroy() {
+        return new Polygon[0];
+    }
+
+    @Override
+    public void paint(Graphics brush) {
+        shape = new Point[] {
+                new Point(0, BASE),
+                new Point(0, BASE + 10),
+                new Point(obj.max_health, BASE + 10),
+                new Point(obj.max_health, BASE)
+        };
+        Point[] points = this.getPoints();
+        int pl = points.length;
+        int[] x = new int[pl];
+        int[] y = new int[pl];
+        for (int i = 0; i < pl; i++) {
+            x[i] = (int)points[i].getX();
+            y[i] = (int)points[i].getY();
+        }
+        brush.setColor(Color.black);
+        brush.fillPolygon(x, y, pl);
+
+
+        shape = new Point[] {
+                new Point(0, BASE),
+                new Point(0, BASE + 10),
+                new Point(obj.health, BASE + 10),
+                new Point(obj.health, BASE)
+        };
+        points = this.getPoints();
+        pl = points.length;
+        x = new int[pl];
+        y = new int[pl];
+        for (int i = 0; i < pl; i++) {
+            x[i] = (int)points[i].getX();
+            y[i] = (int)points[i].getY();
+        }
+        brush.setColor(color.green);
+        brush.fillPolygon(x, y, pl);
     }
 }
