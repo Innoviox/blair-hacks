e2ac1832f3c7eac289be0d8100c81db4fbb70ed6
Innoviox <sarras305@gmail.com>
2017-12-02 13:45:42 -0500

    container classes

--- Tanks/src/Square.java
+++ Tanks/src/Square.java
@@ -1,2 +1,19 @@
-public class Square {
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class Square extends Damagable {
+    public Square(Point[] inShape, Point inPosition, double inRotation, int width, int height, BufferedImage image){
+        super(inShape, inPosition, inRotation, width, height, image, new Random().nextInt(50) + 50);
+    }
+
+    @Override
+    public void move() {
+        this.position.x += Math.random() * 5;
+        this.position.y += Math.random() * 5;
+    }
+
+    @Override
+    public Polygon[] destroy() {
+        return new Polygon[0];
+    }
 }
