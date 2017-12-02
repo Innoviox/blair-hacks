1be1f236f36340ec44bad23f5f1d16998faf980b
FriendlySinusoid <s.f.skarpiphi@gmail.com>
2017-12-02 14:04:16 -0500

    Add update thread

--- Tanks/src/Game.java
+++ Tanks/src/Game.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Game {
 
 	private Canvas canvas;
 
 	private List<Square> squares;
+
+	private Runnable update;
+
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
 	public static void main(String args[]) {
 		Game g = new Game();
@@ -14,15 +21,22 @@ public class Game {
 	}
 
 	public void start() {
-		//TODO implement startup
 		canvas.paint(canvas.getGraphics());
 
+		update = new Runnable() {
+			@Override
+			public void run() {
+				update();
+			}
+		};
 
-
-
+		scheduler.scheduleAtFixedRate(update, 1, 1000/60, TimeUnit.MILLISECONDS);
 
 	}
 
+	public void update() {
+		//TODO update!
+	}
 
 	public Game() {
 		canvas = new Canvas();
