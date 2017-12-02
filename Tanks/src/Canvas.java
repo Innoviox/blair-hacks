53810844d0bc2e6c5b76f05d45c3c66ba68f1be6
FriendlySinusoid <s.f.skarpiphi@gmail.com>
2017-12-02 13:28:40 -0500

    local

---
+++ Tanks/src/Canvas.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JFrame {
+
+	public static final double MAXWIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+	public static final double MAXHEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
+	public Canvas(){
+
+	}
+
+
+}
--- Tanks/src/Game.java
+++ Tanks/src/Game.java
@@ -1,2 +1,12 @@
 public class Game {
+
+	private Canvas canvas;
+
+	public static void main(String args[]) {
+		new Game();
+	}
+
+	public Game() {
+		canvas = new Canvas();
+	}
 }
