64109a7f8522213b184627d167061fd4f873b804
Innoviox <sarras305@gmail.com>
2017-12-02 15:44:39 -0500

    more commit formating

--- Tanks/src/Game.java
+++ Tanks/src/Game.java
@@ -58,11 +58,7 @@ public class Game {
             gameObjects.add(new Square(squarePoints, position, 0));
         }
 
-<<<<<<< HEAD
-        player = new Tank(new Point[] {new Point(0,0), new Point(10,10), new Point(-5,4)},new Point(0,0),0,
-=======
         player = new Tank(new Point[] {new Point(0,0), new Point(0, 10), new Point(10, 10), new Point(10, 0)},new Point(0,0),0,
->>>>>>> a8b4a0f91b3245f18a5b0bf59e42f8fb469d45ce
 		        10,10, ImageIO.read(new File("images/tank_blue.png")),10);
 
 	    canvas = new Canvas(player);
